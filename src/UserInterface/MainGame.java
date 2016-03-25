062eacd744f61b2e8530970bb5a9b0a534ac408f
Supitsara Kruaongarjnukool <BObOBo@10.202.201.211>
2016-03-25 15:40:56 +0700

    maingame ui

--- src/UserInterface/MainGame.java
+++ src/UserInterface/MainGame.java
@@ -2,6 +2,7 @@ package UserInterface;
 
 import javax.swing.JPanel;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 
@@ -20,6 +21,8 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JTextPane;
 import javax.swing.border.LineBorder;
 import java.awt.SystemColor;
+import java.awt.Toolkit;
+
 import javax.swing.border.BevelBorder;
 import javax.swing.border.CompoundBorder;
 
@@ -36,7 +39,7 @@ public class MainGame{
 	private void initialize() {
 		panel = new JPanel();
 		panel.setLayout(new BorderLayout(0, 0));
-		panel.setPreferredSize(new Dimension(1024,768));
+		panel.setPreferredSize(new Dimension(1024,768)); // 768-568-100
 		
 		JPanel top = new JPanel();
 		top.setPreferredSize(new Dimension(1024, 150));
@@ -81,9 +84,10 @@ public class MainGame{
 		player1.setPreferredSize(new Dimension(300,300));
 		
 		JPanel topP1 = new JPanel();
-		topP1.setPreferredSize(new Dimension(300, 50));
+		topP1.setPreferredSize(new Dimension(300, 100));
 		
 		JLabel lblPlaceYourShip = new JLabel("PLACE YOUR SHIPS!");
+		lblPlaceYourShip.setVerticalAlignment(SwingConstants.BOTTOM);
 		lblPlaceYourShip.setFont(new Font("Avenir", Font.BOLD, 20));
 		lblPlaceYourShip.setHorizontalAlignment(SwingConstants.LEFT);
 		topP1.add(lblPlaceYourShip);
@@ -112,14 +116,13 @@ public class MainGame{
 		
 		/*PLAYER2 TABLE*/
 		JPanel rightCol = new JPanel();
-		rightCol.setPreferredSize(new Dimension(300,568));
+		rightCol.setPreferredSize(new Dimension(300,618)); //568+50
 		center.add(rightCol, BorderLayout.EAST);
 		
 		JPanel statusPanel = new JPanel();
 		statusPanel.setPreferredSize(new Dimension(300,50));
 		statusPanel.setLayout( new BorderLayout(0,0));
 		JPanel topP2 = new JPanel();
-	;
 		
 		JPanel bottomP2 = new JPanel();
 		bottomP2.setPreferredSize(new Dimension(300,100));	
@@ -161,16 +164,91 @@ public class MainGame{
 		rightCol.add(player2,BorderLayout.CENTER);
 		rightCol.add(bottomP2, BorderLayout.SOUTH);
 		
+		//score
 		player2.setLayout(new BorderLayout(0, 0));
 		JPanel northPlayer2 = new JPanel();
 		northPlayer2.setPreferredSize(new Dimension(300, 100));
 		player2.add(northPlayer2, BorderLayout.NORTH);
+		northPlayer2.setLayout(new BorderLayout(0, 0));
+		JPanel scoreClient = new JPanel();
+		scoreClient.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		JPanel scoreServer = new JPanel();
+		scoreServer.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		JPanel gap4 = new JPanel(); //vertical gap
+		JPanel gap5 = new JPanel(); //horizontal gap
+		JPanel scorePanel = new JPanel();
+		scorePanel.setPreferredSize(new Dimension(250, 90));
+		scoreClient.setPreferredSize(new Dimension(110,40));
+		scoreServer.setPreferredSize(new Dimension(110,40));
+		gap4.setPreferredSize(new Dimension(50,10));
+		gap5.setPreferredSize(new Dimension(300,10));
+		scorePanel.setLayout(new BorderLayout(0, 0));
+		northPlayer2.add(gap4, BorderLayout.WEST);
+		northPlayer2.add(scorePanel, BorderLayout.CENTER);
+		northPlayer2.add(gap5, BorderLayout.SOUTH);
+		scorePanel.add(scoreClient, BorderLayout.WEST);
+		scorePanel.add(scoreServer, BorderLayout.EAST);
+		//profile player
+		JPanel profileClient = new JPanel();
+		profileClient.setPreferredSize(new Dimension(30,40));
+		profileClient.setBackground(Color.PINK);
+		scoreClient.setLayout(new BorderLayout(0, 0));
+		//scoreClient.add(profileClient, BorderLayout.WEST);
+		
+		JPanel client = new JPanel();
+		scoreClient.add(client, BorderLayout.CENTER);
+		client.setLayout(new BorderLayout(0, 0));
+		
+		JPanel nameClient = new JPanel();
+		nameClient.setBackground(Color.GRAY);
+		nameClient.setPreferredSize(new Dimension(60,30));
+		client.add(nameClient, BorderLayout.NORTH);
+		
+		JLabel lblPlayer = new JLabel("PLAYER1");
+		lblPlayer.setVerticalAlignment(SwingConstants.TOP);
+		lblPlayer.setFont(new Font("Avenir", Font.PLAIN, 12));
+		lblPlayer.setHorizontalAlignment(SwingConstants.CENTER);
+		nameClient.add(lblPlayer);
+		
+		JPanel clientScore = new JPanel();
+		clientScore.setPreferredSize(new Dimension(30, 60));
+		client.add(clientScore, BorderLayout.SOUTH);
+		
+		
+		JPanel profileServer = new JPanel();
+		profileServer.setPreferredSize(new Dimension(30,40));
+		profileServer.setBackground(Color.PINK);
+		scoreServer.setLayout(new BorderLayout(0, 0));
+		scoreServer.add(profileServer, BorderLayout.EAST);
+		
+		JPanel server = new JPanel();
+		scoreServer.add(server, BorderLayout.CENTER);
+		server.setLayout(new BorderLayout(0, 0));
+		
+		JPanel nameServer = new JPanel();
+		server.add(nameServer, BorderLayout.NORTH);
+		
+		JPanel panel_1 = new JPanel();
+		server.add(panel_1, BorderLayout.SOUTH);
+		
+		JPanel vsPanel = new JPanel();
+		scorePanel.add(vsPanel, BorderLayout.CENTER);
+		
+		JLabel lblVs = new JLabel("VS");
+		lblVs.setHorizontalAlignment(SwingConstants.CENTER);
+		lblVs.setFont(new Font("Avenir", Font.PLAIN, 13));
+		vsPanel.add(lblVs);
+		
+		
+	    //Image background = Toolkit.getDefaultToolkit().createImage("Background.png");
+	    //profileClient.drawImage(background, 0, 0, null);
+		
 		
 		JPanel gap3 = new JPanel();
-		gap3.setPreferredSize(new Dimension(100, 250));
+		gap3.setPreferredSize(new Dimension(50, 250));
 		JPanel southPlayer2 = new JPanel();
 		southPlayer2.setBackground(new Color(204, 204, 255));
-		southPlayer2.setPreferredSize(new Dimension(200, 200));
+		southPlayer2.setPreferredSize(new Dimension(250, 250));
 		player2.add(gap3,BorderLayout.WEST);
 		player2.add(southPlayer2, BorderLayout.CENTER);
 		southPlayer2.setLayout(new GridLayout(8, 8, 0, 0));
@@ -186,7 +264,8 @@ public class MainGame{
 		
 		
 		JPanel bottom = new JPanel();
-		bottom.setPreferredSize(new Dimension(1024,100));
+		bottom.setPreferredSize(new Dimension(1024,50));
+		bottom.setBackground(Color.BLACK);
 		panel.add(bottom, BorderLayout.SOUTH);
 	}
 
