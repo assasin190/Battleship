4e5f17dc707e37f6e5e8a9042fa9be230b879325
Supitsara Kruaongarjnukool <BObOBo@192.168.1.63>
2016-03-27 09:50:55 +0700

    ip and port ui update

--- src/UserInterface/MainMenuUI.java
+++ src/UserInterface/MainMenuUI.java
@@ -139,29 +139,47 @@ class ModeSelectDialog extends JDialog{
 		ipTextField.setColumns(10);
 		JLabel ipTextLabel = new JLabel("IP Address:");
 		
+		JPanel gapNorth = new JPanel();
+		gapNorth.setPreferredSize(new Dimension(250,140));
+		north.add(gapNorth, BorderLayout.NORTH);
+		
 		
 		JPanel ipLabel = new JPanel();
-		ipLabel.setPreferredSize(new Dimension(250,200));
+		ipLabel.setPreferredSize(new Dimension(250,30));
 		JPanel ipField = new JPanel();
-		ipField.setPreferredSize(new Dimension(250,200));
-		//ipLabel.setLayout(new BorderLayout());
-		//ipField.setLayout(new BorderLayout());
-		ipLabel.add(ipTextLabel);
-		ipField.add(ipTextField);
+		ipField.setPreferredSize(new Dimension(250,30));
+		ipLabel.setLayout(new BorderLayout());
+		ipField.setLayout(new BorderLayout());
+		ipLabel.add(ipTextLabel,BorderLayout.EAST);
+		ipField.add(ipTextField,BorderLayout.WEST);
 		north.add(ipLabel,BorderLayout.WEST);
 		north.add(ipField,BorderLayout.EAST);
 		
-		ipTextLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		ipTextField.setHorizontalAlignment(SwingConstants.LEFT);
+		//ipTextLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		//ipTextField.setHorizontalAlignment(SwingConstants.LEFT);
 		
 		//South
 		JPanel south = new JPanel();
-		south.setLayout(new FlowLayout());
-		portTextField = new JTextField();
-		JLabel portTextLabel = new JLabel("Port:");
-		south.add(portTextLabel);
-		south.add(portTextField);
+		south.setLayout(new BorderLayout());
+		JPanel portLabel = new JPanel();
+		JPanel portField = new JPanel();
 		
+		portTextField = new JTextField();
+		portTextField.setColumns(10);
+		JLabel portTextLabel = new JLabel("Port:");
+
+		portLabel.setLayout(new BorderLayout());
+		portLabel.add(portTextLabel,BorderLayout.EAST);
+		portField.setLayout(new BorderLayout());
+		portField.add(portTextField,BorderLayout.WEST);
+		portLabel.setPreferredSize(new Dimension(250,30));
+		portField.setPreferredSize(new Dimension(250,30));
+		south.add(portLabel,BorderLayout.WEST);
+		south.add(portField,BorderLayout.EAST);
+		
+		JPanel gapSouth = new JPanel();
+		gapSouth.setPreferredSize(new Dimension(250,200));
+		south.add(gapSouth, BorderLayout.SOUTH);
 		
 		socketPanel.add(north,BorderLayout.NORTH);
 		socketPanel.add(south,BorderLayout.SOUTH);
