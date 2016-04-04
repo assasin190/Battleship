ad5d4c09a3a13572b02460921cff38ae2de15f68
Supitsara Kruaongarjnukool <BObOBo@10.202.255.39>
2016-04-04 15:36:43 +0700

    delete push test

--- src/TestRun2.java
+++ src/TestRun2.java
@@ -10,7 +10,6 @@ public class TestRun2 {
 		//Create a class object
 		MainGameUI game = new MainGameUI();
 		JFrame frame = new JFrame();
-		int hi_lo;
 		
 		//Add class panel
 		frame.add(game.panel);
