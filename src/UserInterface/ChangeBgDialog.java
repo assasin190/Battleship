b40d752e26593a7b88053f931d14e395e097714e
sirawichj <p4728sg2004@hotmail.com>
2016-04-06 22:12:17 +0700

    fix change BG

--- src/UserInterface/ChangeBgDialog.java
+++ src/UserInterface/ChangeBgDialog.java
@@ -1,4 +1,5 @@
 package UserInterface;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -31,85 +32,90 @@ import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
 
-public class ChangeBgDialog extends JPanel{
-	public static final String[] IMAGES = new String[]{
-	        "Bg-play.png",
-	        "Bg-play2.png","Bg-play3.png","Bg-play4.png"
-	    };
-	
+import Game.Main;
+
+public class ChangeBgDialog extends JPanel {
+	public static final String[] IMAGES = new String[] { "Bg-play.png", "Bg-play2.png", "Bg-play3.png",
+			"Bg-play4.png" };
 
 	public static void main(String[] args) {
-		//Create a class object
+		// Create a class object
 		JFrame frame = new JFrame();
 		frame.getContentPane().add(new ChangeBgDialog());
-		frame.setPreferredSize(new Dimension(500,400));
+		frame.setPreferredSize(new Dimension(500, 400));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
 		frame.setResizable(false);
-		
+
 	}
-	
+
 	public ChangeBgDialog() {
-		
+
 		setLayout(new BorderLayout());
-		
-		
+
 		JPanel title = new JPanel();
 		title.setPreferredSize(new Dimension(500, 80));
 		add(title, BorderLayout.NORTH);
 		title.setLayout(new BorderLayout(0, 0));
-		
+
 		JLabel lblChangeBattleshipBackground = new JLabel("CHANGE BACKGROUND");
 		lblChangeBattleshipBackground.setHorizontalAlignment(SwingConstants.CENTER);
 		lblChangeBattleshipBackground.setVerticalAlignment(SwingConstants.BOTTOM);
 		lblChangeBattleshipBackground.setFont(new Font("Arial", Font.BOLD, 16));
 		title.add(lblChangeBattleshipBackground);
-		
+
 		JPanel bgPanel = new JPanel();
 		bgPanel.setPreferredSize(new Dimension(500, 300));
 		add(bgPanel, BorderLayout.SOUTH);
-		
+
 		JPanel gap = new JPanel();
 		gap.setPreferredSize(new Dimension(500, 20));
 		add(gap, BorderLayout.CENTER);
-		
-		
-		JButton bgButton []= new JButton[4];
-		
-		for(int i =0; i<4;i++){
-			bgButton[i]=new JButton(createImageIcon(IMAGES[i],200,120));
+
+		JButton bgButton[] = new JButton[4];
+
+		for (int i = 0; i < 4; i++) {
+			bgButton[i] = new JButton(createImageIcon(IMAGES[i], 200, 120));
 			bgButton[i].setName(i + "");
-			bgButton[i].setPreferredSize(new Dimension(200,120));
-        	bgPanel.add(bgButton[i]); 	
-        	bgButton[i].addMouseListener(new MouseAdapter() {
-				 
-				 @Override
-				 public void mouseClicked(MouseEvent e) {
-					 String name = ((JButton)e.getComponent()).getName();
-					 int index = Integer.parseInt(name); 
-					 MainMenuUI.bg=IMAGES[index];
-				 }
-			 });
-        }
-        
-			
-	
-		
-	
-		
-	
-		
+			bgButton[i].setPreferredSize(new Dimension(200, 120));
+			bgPanel.add(bgButton[i]);
+			bgButton[i].addMouseListener(new MouseAdapter() {
+
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					String name = ((JButton) e.getComponent()).getName();
+					int index = Integer.parseInt(name);
+
+					
+					//MainMenuUI.bg = IMAGES[index];
+					
+					System.out.println("print in ChangeBgDialog");
+					MainMenuUI.changeBg(IMAGES[index]);
+					
+					//MainMenuUI.bg = IMAGES[index];
+
+					//MainMenuUI.this.revalidate();
+
+					// MainMenuUI.this.repaint();
+					//Main.repaint();
+					
+					
+
+				
+				}
+			});
+		}
+
 	}
-	
-	     
+
 	public static ImageIcon createImageIcon(String path, int width, int height) {
 		Image img = null;
 		try {
 			img = ImageIO.read(new File(path));
 		} catch (IOException e) {
 		}
-		
+
 		Image resizedImage = img.getScaledInstance(width, height, 0);
 		return new ImageIcon(resizedImage);
 	}
