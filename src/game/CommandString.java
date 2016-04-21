d04cad8b9e85f590ec3697c12efaf61e0ccf7563
Hades Reflection <assanee@Assanees-iMac.local>
2016-04-22 03:54:26 +0700

    push RESET BUTTON

--- src/game/CommandString.java
+++ src/game/CommandString.java
@@ -8,6 +8,7 @@ public class CommandString {
 	public static final String CLIENT_START_GAME_SETUP = "CLIENT_START_GAME_SETUP";
 	public static final String CLIENT_LOSE = "CLIENT_LOSE";
 	public static final String CLIENT_REQUEST_NEW_GAME = "CLIENT_REQUEST_NEW_GAME";
+	public static final String CLIENT_RESET_GAME = "CLIENT_RESET GAME";
 	
 	public static final String SERVER_OTHER_CLIENT_NOT_AVAILABLE = "SERVER_OTHER_CLIENT_NOT_AVAILABLE";
 	public static final String SERVER_OTHER_CLIENT_AVAILABLE = "SERVER_OTHER_CLIENT_AVAILABLE";
@@ -18,8 +19,7 @@ public class CommandString {
 	public static final String SERVER_GRANT_TURN = "SERVER_GRANT_TURN";
 	public static final String SERVER_INDICATE_YOU_WIN = "SERVER_INDICATE_YOU_WIN";
 	public static final String SERVER_INDICATE_YOU_LOSE = "SERVER_INDICATE_YOU_LOSE";
-	public static final String SERVER_REQUEST_BOARD_EXCHANGE = "SERVER_REQUEST_BOARD_EXCHANGE";
-	public static final String CLIENT_SUBMIT_BOARD_EXCHANGE = "CLIENT_SUBMIT_EXCHANGE_REQUEST";
+	public static final String SERVER_RESET_GAME = "SERVER_RESET_GAME";
 	
 	public static final String MATCH_SERVER_OTHER_MATCHING_NOT_AVAILABLE = "MATCH_SERVER_OTHER_MATCHING_NOT_AVAILABLE";
 	public static final String MATCH_SERVER_OTHER_MATCHING_AVAILABLE = "MATCH_SERVER_OTHER_MATCHING_AVAILABLE";
